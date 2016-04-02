bf9afa7dad1e301e3ca3c1f829ac2273947dad99
Danny Hvam <dkhvam@gmail.com>
2016-04-02 23:09:01 +0200

    Create one-page-print.user.js

---
+++ one-page-print.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         BowlingPortalen - One page print
+// @namespace    https://github.com/oizo/bowlingportalen/
+// @downloadURL  https://raw.githubusercontent.com/
+// @version      0.1
+// @description  Make all the printable league game tables from BowlingPortalen fit a standard A4 paper
+// @author       Danny Hvam
+// @match        http://bowlingportalen.dk/DBwF/HoldTurnering/UdskrivHoldkamp/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML += ".printpage .matchresultschema .toprow, .printpage .matchresultschema .totalsrow, .printpage .matchresultschema .pointsrow {";
+    style.innerHTML += "	height: 5mm;";
+    style.innerHTML += "}";
+    style.innerHTML += ".printpage .matchresultschema .printplayer div, .printpage .matchresultschema .printplayer5 div {";
+    style.innerHTML += "	padding-top: 0mm !important;";
+    style.innerHTML += "	height: 5mm !important;";
+    style.innerHTML += "}";
+    head.appendChild(style);
+
+})();
